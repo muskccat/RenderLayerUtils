cdbd05678f565e23c3efea9918c148efa42941ff
muskccat <muskccat@naver.com>
2016-07-19 19:02:45 +0900

    플레인을 카메라에 parent 등
    near clip plane과 far clip plane을 생성시켜 카메라에 parent 시키는 과정 추가
    폴리곤 Plane을 생성하고, 카메라엔 Null transform 노드 2개를 Parent시킨 후에 여기에 폴리곤 plane을
    인스턴스로 parent시킴
    
    이 과정 중에 채널 속성들을 잠그고, 숨기는 부분이 지속적으로 반복됨
    채널을 조절하는 함수를 만들 필요가 있

--- clipPlane_creator.py
+++ clipPlane_creator.py
@@ -25,14 +25,88 @@ def make_plane(name):
     cmds.setAttr(pShape +".doubleSided", 0)
 
     # cmds.setAttr(plane+".hiddenInOutliner", 1)
+    # It doesn't show in Outliner. (On purpose, it was hidden)
+    
     cmds.setAttr(plane+".overrideEnabled",1)
     cmds.setAttr(plane+".overrideShading",0)
     cmds.setAttr(plane+".overrideColor",17)
-
+    cmds.setAttr(plane+".rx", 90)
+    cmds.setAttr(plane + ".tx", lock=True, k=False, channelBox = False)
+    cmds.setAttr(plane + ".ty", lock=True, k=False, channelBox = False)
+    cmds.setAttr(plane + ".tz", lock=True, k=False, channelBox = False)
+    cmds.setAttr(plane + ".rx", lock=True, k=False, channelBox = False)
+    cmds.setAttr(plane + ".ry", lock=True, k=False, channelBox = False)
+    cmds.setAttr(plane + ".rz", lock=True, k=False, channelBox = False)
+    cmds.setAttr(plane + ".sx", lock=True, k=False, channelBox = False)
+    cmds.setAttr(plane + ".sy", lock=True, k=False, channelBox = False)
+    cmds.setAttr(plane + ".sz", lock=True, k=False, channelBox = False)
+    
+    
     planes = (plane, pShape)
 
     return planes
+    
+def g_plane(*args):
+    
+    # create a null & define name
+    for i, x in enumerate(args):
+        cmds.select(cl=True)
+        x = cmds.Group()
+        n1 = cmds.ls(sl=True)
+        cmds.rename(n1,args[i])
 
+        
+    return args
+    
+
+    
+            
+    
+def c_plane(*ars):
+    name = cmds.textScrollList('cam_sel', q=True, si=True)
+    print name
+    
+    def make_tex(*args):
+        tex = []
+        for x in args:
+            x = cmds.textCurves( ch=False, f="Arial|w400|h-11", t=x)
+            tex.append(x)
+        return tex
+    
+    if name==None : print "Please select a camera."
+    else :
+        if not(cmds.objExists(name[0] + "_clipPlane")):
+            cp = make_plane(name[0]+"_clipPlane")
+            g = g_plane(name[0] + "_nearClipping", name[0]+"_farClipping")
+            t = make_tex("NEAR CLIP PLANE", "FAR CLIP PLANE")
+            
+            for i,x in enumerate(g) :
+                cmds.parent(x,name[0])
+                cmds.setAttr(x+".translate",0,0,0)
+                cmds.setAttr(x+".rotate",0,0,0)
+                cmds.setAttr(x+".scale",1,1,1)
+
+                cmds.parent(t[i], x)
+                tex = t[i]
+                cmds.setAttr(tex[0]+".translate",0,0,0)
+                cmds.setAttr(tex[0]+".rotate",0,0,0)
+                cmds.setAttr(tex[0]+".scale",.1,.1,.1)
+                cmds.setAttr(tex[0] + ".tx", lock=True, k=False, channelBox = False)
+                cmds.setAttr(tex[0] + ".ty", lock=True, k=False, channelBox = False)
+                cmds.setAttr(tex[0] + ".tz", lock=True, k=False, channelBox = False)
+                cmds.setAttr(tex[0] + ".rx", lock=True, k=False, channelBox = False)
+                cmds.setAttr(tex[0] + ".ry", lock=True, k=False, channelBox = False)
+                cmds.setAttr(tex[0] + ".rz", lock=True, k=False, channelBox = False)
+                cmds.setAttr(tex[0] + ".sx", k=False, channelBox = False)
+                cmds.setAttr(tex[0] + ".sy", k=False, channelBox = False)
+                cmds.setAttr(tex[0] + ".sz", k=False, channelBox = False)
+                
+                cmds.select(cp[0],r=True)
+                cmds.select(x,add=True)
+                cmds.parent(add=True)
+                
+                
+            
 
 
 def clip_plane_creator(*args):
@@ -44,17 +118,18 @@ def clip_plane_creator(*args):
     cmds.window(gui,t="Clip Plane Creator v1.0")
     cmds.columnLayout(adjustableColumn = True)
     
+    # Get perspective camera (Not orthographic)
     cms = cmds.ls(typ="camera")
     cm_list = []
-    for x in cms:
-        ortho = cmds.getAttr(x+".orthographic")
-        p = cmds.listRelatives(x,p=True)
-        if not(ortho) : cm_list.append(p[0])
+    cmt = lambda x:cmds.listRelatives(x, p=True)    # get camera's transform node
+    cm_list = filter(lambda x:not(cmds.getAttr(x+".orthographic")), cms)    # if orthographic is true, doesn't append "cm_list var"
+    cm_list = cmt(cm_list)
+    
     
     cmds.text("Camera List", h = 30)
     cmds.textScrollList('cam_sel',append = cm_list,ams=False)
     cmds.separator()
-        
+    cmds.button('bCreatePlane', label = "Create Clip Plane", width = 200, c = c_plane)
     cmds.window(gui, e=1, width=360, height = 120)
     cmds.showWindow(gui)
     
