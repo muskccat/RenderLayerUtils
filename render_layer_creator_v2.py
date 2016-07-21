0ddafc7624b5946c990db206183766c6b9ad4b74
muskccat <muskccat@naver.com>
2016-07-21 19:43:38 +0900

    렌더레이어 생성 스크립트 수정
    Empty를 Diffuse로 수정
    카메라 선택란 추가
    레이어 순서 변경
    매터리얼 적용 및 DefaultRenderGlobal 옵션 변경 적용 예정

--- render_layer_creator_v2.py
+++ render_layer_creator_v2.py
@@ -1,13 +1,13 @@
 # coding : euc-kr
 # MARO render-layer creation script
 #
-# 2016.07.18
-# ver 2.0
+# 2016.07.21
+# ver 2.1
 # by Musky
 
 import maya.cmds as cmds
 import maya.mel as mel
-
+import types
 
 # SHADER Creator
 def make_shd(name, rgb, type):
@@ -41,7 +41,7 @@ def make_shd(name, rgb, type):
     def set_ao(n):
         oc = mel.eval("mrCreateCustomNode -asTexture \"\" mib_amb_occlusion")
         cmds.connectAttr(oc+".outValue", n+".outColor", f=True)
-        cmds.setAttr(oc+".samples", 264)
+        cmds.setAttr(oc+".samples", 256)
         cmds.setAttr(oc+".max_distance", 1.0)
 
     
@@ -101,26 +101,54 @@ def shd_creation():
 # Create Render Layer
 def make_layer(name,sf,ef, r_type):
     rl_name = name
+    sels = cmds.ls(sl=True)
 
-    l = cmds.createRenderLayer(n=rl_name, number=2,noRecurse=True)
-    cmds.editRenderLayerGlobals(crl=l)
-    cmds.editRenderLayerAdjustment("defaultRenderGlobals.imageFilePrefix")
-    cmds.editRenderLayerAdjustment("defaultRenderGlobals.imageFormat")
-   
-    drg = "defaultRenderGlobals"
-    cmds.setAttr(drg+".imageFilePrefix",rl_name,typ="string")
-    cmds.setAttr(drg+".imageFormat",32)
-    cmds.setAttr(drg + ".startFrame", sf)
-    cmds.setAttr(drg + ".endFrame", ef)
+    r_cam = cmds.textScrollList('r_cam', q=True, si=True)
+    cs = lambda x:cmds.listRelatives(x,shapes=True)
     
-    if (r_type == "AO"):
-        cmds.editRenderLayerAdjustment(drg+".currentRenderer")
-        cmds.setAttr(drg+".ren","mentalRay",typ="string")
+    #check object exist
+    if not(cmds.objExists(name)):
+        l = cmds.createRenderLayer(n=rl_name, number=2,noRecurse=True)
+        cmds.editRenderLayerGlobals(crl=l)
+        cmds.editRenderLayerAdjustment("defaultRenderGlobals.imageFilePrefix")
+        cmds.editRenderLayerAdjustment("defaultRenderGlobals.imageFormat")
+           
+        drg = "defaultRenderGlobals"
+        cmds.setAttr(drg+".imageFilePrefix",rl_name,typ="string")
+        cmds.setAttr(drg+".imageFormat",32)
+        cmds.setAttr(drg + ".startFrame", sf)
+        cmds.setAttr(drg + ".endFrame", ef)
+        
+        if (r_type == "AO"):
+            cmds.editRenderLayerAdjustment(drg+".currentRenderer")
+            cmds.setAttr(drg+".ren","mentalRay",typ="string")
 
-     
+        put_obj(sels, rl_name)
+    else: l = rl_name
+    
+    
+    # Collect Camera data
+    a_cam = cm_list()
+    a_cam = a_cam[0]
+    
+    # All camera's renderable attribute is OFF.
+    for ac in a_cam:
+        cmds.setAttr(ac+".renderable", 0)
+   
+    # Turn on "renderble" : selected camera.
+    if not(type(r_cam) == types.NoneType):
+        for rc in r_cam:
+            x = cs(rc)
+            cmds.setAttr(x[0]+".renderable", 1)
     return l
-
-
+    
+# selected object -> render layer
+def put_obj(obj,rl):
+    for x in obj:
+        cmds.editRenderLayerMembers(rl,x,noRecurse=True)
+        
+        
+# Get Render Layer Name
 def get_name(type):
     
     name =  "EP_" + cmds.textFieldGrp('ep_field',q=True,text=True) +  "_SC" + cmds.textFieldGrp('sc_field',q=True,text=True) +  "_C"  + cmds.textFieldGrp('cn_field',q=True,text=True) +"_"   + cmds.textFieldGrp('ex_field',q=True,text=True) 
@@ -132,16 +160,16 @@ def get_name(type):
 
     return name
 
-
+# button commands
 def bRgb_mask(*args):
-    type = "RGB_Mask"
+    type = "MASK"
     name = get_name(type)    
     sf = int(cmds.textFieldGrp('start_frame', q=True, tx=True))
     ef = int(cmds.textFieldGrp('end_frame', q=True, tx=True))
     make_layer(name, sf, ef, type)
 
 def bNormal(*args):
-    type = "Normal"
+    type = "NRM"
     name = get_name(type)    
     sf = int(cmds.textFieldGrp('start_frame', q=True, tx=True))
     ef = int(cmds.textFieldGrp('end_frame', q=True, tx=True))
@@ -156,21 +184,39 @@ def bAO(*args):
 
 
 def bZDepth(*args):
-    type = "zDepth"
+    type = "ZD"
     name = get_name(type)    
     sf = int(cmds.textFieldGrp('start_frame', q=True, tx=True))
     ef = int(cmds.textFieldGrp('end_frame', q=True, tx=True))
     make_layer(name, sf, ef, type)    
     
-def bEmpty(*args):
-    type = ""
+def bDiffuse(*args):
+    type = "DF"
     name = get_name(type)    
     sf = int(cmds.textFieldGrp('start_frame', q=True, tx=True))
     ef = int(cmds.textFieldGrp('end_frame', q=True, tx=True))
-    make_layer(name, sf, ef, type)    
+    make_layer(name, sf, ef, type)   
+    
+def bAll(*args):
+    bDiffuse()
+    bNormal()
+    bRgb_mask()
+    bZDepth()
+    bAO()
+    
+
+# Get camera list // only perspective
+def cm_list():
+    cms = cmds.ls(typ='camera')
+    cmt = lambda x:cmds.listRelatives(x,p=True)
+
+    cm_list = filter(lambda x:not(cmds.getAttr(x+".orthographic")) , cms)
+    cmt = cmt(cm_list)
+    return (cm_list, cmt)
+   
 
 
-
+# Main GUI
 def mrl_base_gui(*ars):
     gui = 'mrl_gui'
     
@@ -181,34 +227,40 @@ def mrl_base_gui(*ars):
     shd_creation() # if default shader don't exist then create new shader.
     cmds.columnLayout(adjustableColumn = True)
     
+    cmds.text("Set Render Layer Name",align="center",h=25, bgc = (.3,.1,.1))
     cmds.textFieldGrp('ep_field', label='Episode',columnAlign=(1,'both'),columnWidth=(1,80))
     cmds.textFieldGrp('sc_field',label='Scene Number', columnAlign=(1,'both'),columnWidth=(1,80))
     cmds.textFieldGrp('cn_field', label='Cut Number', columnAlign=(1,'both'),columnWidth=(1,80))
     cmds.textFieldGrp('ex_field',label='Explanation',  columnAlign=(1,'both'),columnWidth=(1,80))
     cmds.checkBox('c_box', label = 'Use a custom name')
     cmds.textFieldGrp('cus',label='Custom',  columnAlign=(1,'both'),columnWidth=(1,80))
-    cmds.separator()
-        
+            
     sf = int(cmds.playbackOptions(q=True,ast=True))
     ef = int(cmds.playbackOptions(q=True,aet=True))
+    cmds.text("Set Time Range",align="center",h=18, bgc = (.3,.1,.1))
     cmds.textFieldGrp('start_frame',label='Start Frame',tx = sf,columnAlign=(1,'both'),columnWidth=(1,80))  
     cmds.textFieldGrp('end frame', label='End Frame', tx = ef, columnAlign=(1,'both'),columnWidth=(1,80))
+    cms = cm_list()
+    cmds.text("Choose Render Camera",align="center",h=25, bgc = (.3,.1,.1))
+    cmds.textScrollList('r_cam', append = cms[1],ams=True, h = 120)
     
+    cmds.text("Create layer",align="center",h=22, bgc = (.3,.1,.1))
     cmds.rowColumnLayout(numberOfRows=2)
-    cmds.button('rgb_mask', label= "RGB Mask", width=120, c=bRgb_mask)
-    cmds.button('normal', label= "Normal", width=120, c=bNormal)
-    cmds.button('AO', label= "AO", width=120, c=bAO)
-    cmds.button('zDepth', label= "zDepth", width=120, c=bZDepth)
-    cmds.button('empty', label= "empty", width=120, c=bEmpty)
+    cmds.button('diffuse', label= "DF", width=120,bgc=(.7,0.7,0.7), c=bDiffuse)
+    cmds.button('normal', label= "NRM", width=120, bgc=(.7,0.7,0.7),c=bNormal)
+    cmds.button('rgb_mask', label= "MASK", width=120,bgc=(.7,0.7,0.7), c=bRgb_mask)
+    cmds.button('zDepth', label= "ZD", width=120,bgc=(.7,0.7,0.7), c=bZDepth)
+    cmds.button('AO', label= "AO", width=120, bgc=(.7,0.7,0.7),c=bAO)
+    cmds.button('all', label= "ALL", width=120, bgc=(.9,0.8,0.7),c=bAll)    
+    cmds.setParent("..")
+
+
+    cmds.button( label ="Refresh",width=120,bgc=(0.3,.5,0.2), command=mrl_base_gui)
     
-    cmds.button( label ="Refresh",width=120, command=mrl_base_gui)
     cmds.window(gui, e=1, width=360, height = 120)
     cmds.showWindow(gui)
 
 
+# GUI execute
 mrl_base_gui()
-cmds.select(cl=True)
     
-    
-
-        
