a62539ee3e53bde3960fa19757e187d25ae42537
muskccat <muskccat@naver.com>
2016-07-21 19:45:03 +0900

    렌더레이어의 오브젝트 Add/Remove
    Object to render layer를 대대적으로 변경
    씬 내에 존재하는 렌더 레이어를 불러온 후,
    렌더레이어를 선택하면 레이어 내에 연결된 오브젝트들을 표시
    
    선택한 오브젝트들을 레이어 내에 집어넣는 기능까지 추가

--- object_to_render_layer.py
+++ object_to_render_layer.py
@@ -1,17 +1,59 @@
 # coding : euc-kr
 # MARO Object to render layer
 # 
-# 2016.07.13
-# ver 1.0
+# 2016.07.21
+# ver 1.5
 # by musky
 
 import maya.cmds as cmds
 import maya.mel as ml
+import types
 
 
-def put_obj(obj,rl):
-    for x in obj:
-        cmds.editRenderLayerMembers(rl[0],x,noRecurse=True)
+def put_obj(*args):    
+    rl = cmds.textScrollList('rls', q=True,si=True)
+    if type(rl) !=types.NoneType:
+        sels = cmds.textScrollList('sels',q=True,si=True)
+        if type(sels) !=types.NoneType:
+            for y in rl:
+                for x in sels:
+                    cmds.editRenderLayerMembers(y,x,noRecurse=True)
+            rl_obj_list()
+                
+def rl_obj_list(*args):    
+    rl = cmds.textScrollList('rls', q=True,si=True)
+    if type(rl) !=types.NoneType:
+        obj = get_obj(rl)
+        if type(obj) != types.NoneType : cmds.textScrollList('objs',e=True, ra=True,append = obj )
+        else : cmds.textScrollList('objs',e=True, ra=True,append = "")
+    
+def sel_objs(*args):
+    sels = cmds.ls(sl=True)
+    cmds.textScrollList('sels',e=True, ra=True,append = sels )
+    
+   
+def get_obj(rl):
+    if type(rl)!=types.NoneType:
+        rList =[]
+        for x in rl:
+            rInfo = x+".renderInfo"
+            li = cmds.listConnections(rInfo, d=True)
+            if type(li) != types.NoneType: rList +=li;
+    
+        return rList
+    else : return rl
+
+def remove_objs(*ars):
+        
+    rl = cmds.textScrollList('rls', q=True,si=True)
+    if type(rl) !=types.NoneType:
+        obj = cmds.textScrollList('objs',q=True,si=True)
+        if type(obj) != types.NoneType:
+            for r in rl:
+                for x in obj:
+                    cmds.editRenderLayerMembers(r,x,remove=True,nr=True)
+            rl_obj_list()
+    
 
 def rgb_base_gui(*args):
 
@@ -20,21 +62,27 @@ def rgb_base_gui(*args):
         
     cmds.window('rgb_gui', t="Object to Render layer")
     cmds.columnLayout(adjustableColumn = True)
-    cmds.rowColumnLayout(numberOfColumns=2)
+    cmds.rowColumnLayout(numberOfColumns=3)
+    cmds.text("Render Layer", h= 20, bgc= (0.3,0.6,0.8))
+    cmds.text("Added Object", h= 20, bgc= (0.7,0.6,0.6))
+    cmds.button("Selected Object", h = 20, bgc =(0.7,0.3,0.3),c = sel_objs)
+    
     sels = cmds.ls(sl=1)
     rls = cmds.ls(typ="renderLayer")
-    rl = []
-    for x in rls:
-        if x != "defaultRenderLayer":
-            rl.append(x)
+    rl = filter(lambda x:x !="defaultRenderLayer", rls)            
 
+    rls = cmds.textScrollList('rls', append=rl,ams=True,  sc = rl_obj_list)
+    objs = cmds.textScrollList('objs', append=sels,ams=True,height = 450,bgc = (0.3,0.3,0.5))
+    sels = cmds.textScrollList('sels', append=sels,ams=True,height = 450)
     
-    objs = cmds.textScrollList(append=sels,ams=True,height = 450)
-    rls = cmds.textScrollList(append=rl,ams=False)
+    cmds.button("REFRESH", c= rgb_base_gui)
+    cmds.button("REMOVE", c=remove_objs,bgc = (0.7,0.6,0.6))
+    cmds.button("ASSIGN", c= put_obj, bgc = (0.7,0.3,0.3))
+    
+
         
     cmds.rowColumnLayout(numberOfRows=2)
-    cmds.button( label = "REFRESH",width = 150,command = rgb_base_gui)
-    cmds.button( label = "Assign",width = 150, command = lambda x:put_obj(cmds.textScrollList(objs,q=True,si=True), cmds.textScrollList(rls,q=True,si=True)))
+
     cmds.window('rgb_gui', e=1, width=450, height = 250)
     cmds.showWindow('rgb_gui')
     
