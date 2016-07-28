1fc1871f5320b36cad9c98049d6cea003ea02ed1
muskccat <muskccat@naver.com>
2016-07-28 19:25:53 +0900

    기존의 이름을 가져와 텍스트란에 표시

--- render_layer_name_changer.py
+++ render_layer_name_changer.py
@@ -16,7 +16,7 @@ def ch_name(*args):
     if type(name) != types.NoneType:
         cmds.rename(name[0],n_name)
         cmds.editRenderLayerGlobals(crl=n_name)
-        cmds.setAttr("defaultRenderGlobals.imageFilePrefix", n_name+"_"+i_name, typ="string")
+        cmds.setAttr("defaultRenderGlobals.imageFilePrefix", i_name, typ="string")
         rl = cmds.ls(typ="renderLayer")
         rls = filter (lambda x:x !="defaultRenderLayer", rl)
         cmds.textScrollList('rl_list', e=True,ra=True,append =rls ,ams=False)
@@ -27,6 +27,9 @@ def img_name(*args):
         cmds.editRenderLayerGlobals(crl=name[0])
         i_n = cmds.getAttr("defaultRenderGlobals.imageFilePrefix")
         cmds.textScrollList('img_list', e=True,ra=True,ams=False,append=i_n)
+       
+        cmds.textFieldGrp('ch_name', e=True, tx = name[0])
+        cmds.textFieldGrp('ch_fname', e=True, tx = name[0])
 
 # Main GUI
 def ren_gui(*ars):
@@ -38,7 +41,7 @@ def ren_gui(*ars):
     cmds.window(gui,t="Render Layer name changer v1.0")
     cmds.columnLayout(adjustableColumn = True)
     
-    cmds.text("Select Render Layer", bgc=(.3,.1,.1), h = 25)
+    cmds.button("Select Render Layer", bgc=(.3,.1,.1), h = 25,c=ren_gui)
     
     rl = cmds.ls(typ="renderLayer")
     rls = filter (lambda x:x !="defaultRenderLayer", rl)
@@ -48,14 +51,9 @@ def ren_gui(*ars):
     cmds.textScrollList('img_list', append="", ams=False)
     
     cmds.setParent("..")    
-    cmds.button('bCh', label="RENDER LAYER RENAME", bgc=(0.5,0.7,0.9), c=ch_name)    
+    cmds.button('bCh', label="RENDER LAYER RENAME", bgc=(0.5,0.7,0.9), c=ch_name)   
     cmds.textFieldGrp('ch_name', label="Input new name:",cal=(1,"left") ,cw=(1,95))
     cmds.textFieldGrp('ch_fname', label="Img name suffix:",cal=(1,"left") ,cw=(1,95))
-    
-    
-
-    
-    
         
     cmds.window(gui, e=1, width=360, height = 120)
     cmds.showWindow(gui)
