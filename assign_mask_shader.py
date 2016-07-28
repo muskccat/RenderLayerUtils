2d6e26aa796126ab23b043d6462774d3cebe24a7
muskccat <muskccat@naver.com>
2016-07-28 19:23:22 +0900

    블랙매트 버튼 추가

--- assign_mask_shader.py
+++ assign_mask_shader.py
@@ -10,7 +10,7 @@ import maya.mel as mel
 
 
 def assign_shd(obj,c):
-    color = ["M_RED_mat",  "M_GREEN_mat", "M_BLUE_mat"]
+    color = ["M_RED_mat",  "M_GREEN_mat", "M_BLUE_mat", "M_BLACK_mat"]
     for x in obj:
         #cmds.sets(color[c],e=True, forceElement=True)
         cmds.select(x,r=1)
@@ -24,11 +24,13 @@ def assign_shd_gui(*args):
     cmds.window('assign_gui',t = "Assign Mask Shader",rtf=True)
     cmds.columnLayout(adjustableColumn = True)
 
-    cmds.rowColumnLayout(numberOfRows=3)
-    cmds.button( label = "RED",width = 350,height=120,bgc=(0.6,0.2,0.2),command =  lambda x:assign_shd( cmds.ls(sl=1) ,0) )
-    cmds.button( label = "GREEN",width = 350,height=120,bgc=(0.2,0.6,0.2),command =  lambda x:assign_shd( cmds.ls(sl=1),1) )
-    cmds.button( label = "BLUE",width = 350,height=120,bgc=(0.2,0.2,0.6),command =  lambda x:assign_shd( cmds.ls(sl=1) ,2) )
-    cmds.window('assign_gui',e=1, width=350,height=360)
+    cmds.rowColumnLayout(numberOfRows=4)
+    cmds.button( label = "RED",width = 350,height=60,bgc=(0.6,0.2,0.2),command =  lambda x:assign_shd( cmds.ls(sl=1) ,0) )
+    cmds.button( label = "GREEN",width = 350,height=60,bgc=(0.2,0.6,0.2),command =  lambda x:assign_shd( cmds.ls(sl=1),1) )
+    cmds.button( label = "BLUE",width = 350,height=60,bgc=(0.2,0.2,0.6),command =  lambda x:assign_shd( cmds.ls(sl=1) ,2) )
+    cmds.button( label = "BLACK",width = 350,height=60,bgc=(0.,0,0),command =  lambda x:assign_shd( cmds.ls(sl=1) ,3) )
+    
+    cmds.window('assign_gui',e=1, width=350,height=240)
     cmds.showWindow('assign_gui')
     
     
